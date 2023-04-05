0ebdfd0bffd5511ca0dae5e5632eee3f4c25cf14
mlugg <mlugg@mlugg.co.uk>
2023-04-05 18:04:56 +0100

    Improve progress reporting, add inplace option, don't stop on bad demos

--- src/main.zig
+++ src/main.zig
@@ -5,61 +5,105 @@ fn err(comptime fmt: []const u8, args: anytype) u8 {
     return 1;
 }
 
+fn countDirFiles(dir: std.fs.IterableDir, ally: std.mem.Allocator) !u32 {
+    var walker = try dir.walk(ally);
+    defer walker.deinit();
+
+    var count: u32 = 0;
+    while (try walker.next()) |ent| {
+        if (ent.kind == .File) {
+            count += 1;
+        }
+    }
+
+    return count;
+}
+
 pub fn main() u8 {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const ally = arena.allocator();
 
     const args = std.process.argsAlloc(ally) catch return err("oom", .{});
-    if (args.len != 2 and args.len != 3) {
+    if (args.len < 2) {}
+
+    var in_name: ?[]const u8 = null;
+    var out_name: ?[]const u8 = null;
+    var inplace: bool = false;
+
+    for (args[1..]) |arg| {
+        if (std.mem.eql(u8, arg, "-I")) {
+            inplace = true;
+        } else if (in_name == null) {
+            in_name = arg;
+        } else if (out_name == null) {
+            out_name = arg;
+        } else {
+            return err("unrecognised argument {s}", .{arg});
+        }
+    }
+
+    if (in_name == null) {
         return err(
-            "usage: {s} <in name> [out name]",
+            \\usage: {s} [options] <in name> [out name]
+            \\options:
+            \\    -I   modify files inplace
+        ,
             .{if (args.len > 0) args[0] else "demofixup"},
         );
     }
 
-    const in_name = args[1];
-    const out_name = if (args.len == 3)
-        args[2]
-    else if (std.mem.endsWith(u8, in_name, ".dem") or std.mem.endsWith(u8, in_name, ".DEM"))
-        std.fmt.allocPrint(ally, "{s}_fixed.dem", .{in_name[0 .. in_name.len - 4]}) catch return err("oom", .{})
-    else
-        std.fmt.allocPrint(ally, "{s}_fixed", .{std.mem.trimRight(u8, in_name, "/\\")}) catch return err("oom", .{});
+    if (inplace) {
+        if (out_name != null) {
+            return err("cannot specify output dir with -I", .{});
+        } else {
+            out_name = in_name;
+        }
+    } else if (out_name == null) {
+        if (std.mem.endsWith(u8, in_name.?, ".dem") or std.mem.endsWith(u8, in_name.?, ".DEM")) {
+            out_name = std.fmt.allocPrint(ally, "{s}_fixed.dem", .{in_name.?[0 .. in_name.?.len - 4]}) catch return err("out of memory", .{});
+        } else {
+            out_name = std.fmt.allocPrint(ally, "{s}_fixed", .{std.mem.trimRight(u8, in_name.?, "/\\")}) catch return err("out of memory", .{});
+        }
+    }
 
-    const stat = std.fs.cwd().statFile(in_name) catch |e| return err("error on stat {s}: {}", .{ in_name, e });
+    const stat = std.fs.cwd().statFile(in_name.?) catch |e| return err("error on stat {s}: {}", .{ in_name.?, e });
     switch (stat.kind) {
         .File => {
-            var in_file = std.fs.cwd().openFile(in_name, .{}) catch |e| return err("failed to open {s}: {}", .{ in_name, e });
-            defer in_file.close();
-            var out_file = std.fs.cwd().createFile(out_name, .{}) catch |e| return err("failed to open {s}: {}", .{ out_name, e });
-            defer out_file.close();
-            fixupDemo(&in_file, &out_file, ally) catch |e| return err("failed to fixup {s}: {}", .{ in_name, e });
-            std.io.getStdOut().writer().print("successfully wrote {s}!\n", .{out_name}) catch {};
+            var progress: std.Progress = .{};
+
+            const root_prog_node = progress.start("run fixup", 1);
+            defer root_prog_node.end();
+
+            fixupFile(ally, root_prog_node, std.fs.cwd(), std.fs.cwd(), in_name.?, out_name.?) catch |e| {
+                std.io.getStdOut().writer().print("failed to fixup {s}: {}", .{ in_name.?, e }) catch {};
+                return 1;
+            };
+
+            std.io.getStdOut().writer().print("successfully wrote {s}!\n", .{out_name.?}) catch {};
         },
         .Directory => {
-            var in_dir = std.fs.cwd().openIterableDir(in_name, .{}) catch |e| return err("failed to open dir {s}: {}", .{ in_name, e });
+            var in_dir = std.fs.cwd().openIterableDir(in_name.?, .{}) catch |e| return err("failed to open dir {s}: {}", .{ in_name.?, e });
             defer in_dir.close();
 
-            var out_dir = std.fs.cwd().makeOpenPath(out_name, .{}) catch |e| return err("failed to open dir {s}: {}", .{ out_name, e });
+            var out_dir = std.fs.cwd().makeOpenPath(out_name.?, .{}) catch |e| return err("failed to open dir {s}: {}", .{ out_name.?, e });
             defer out_dir.close();
 
-            var walker = in_dir.walk(ally) catch |e| return err("failed to walk dir {s}: {}", .{ in_name, e });
-            while (walker.next() catch |e| return err("failed to iterate dir: {}", .{e})) |ent| {
-                if (ent.kind != .File) continue;
+            var failed = std.ArrayList([]const u8).init(ally);
 
-                // create the output dir
-                if (std.fs.path.dirname(ent.path)) |dirname| {
-                    out_dir.makePath(dirname) catch |e| return err("failed to make out dir {s}: {}", .{ dirname, e });
-                }
-
-                var in_file = ent.dir.openFile(ent.basename, .{}) catch |e| return err("failed to open {s}{c}{s}: {}", .{ in_name, std.fs.path.sep, ent.path, e });
-                defer in_file.close();
-                var out_file = out_dir.createFile(ent.path, .{}) catch |e| return err("failed to open {s}{c}{s}: {}", .{ out_name, std.fs.path.sep, ent.path, e });
-                defer out_file.close();
-                fixupDemo(&in_file, &out_file, ally) catch |e| return err("failed to fixup {s}{c}{s}: {}", .{ in_name, std.fs.path.sep, ent.path, e });
-                std.io.getStdOut().writer().print("successfully wrote {s}{c}{s}!\n", .{ out_name, std.fs.path.sep, ent.path }) catch {};
+            switch (fixupDir(in_dir, out_dir, &failed, ally)) {
+                0 => {},
+                else => |ret| return ret,
             }
-            std.io.getStdOut().writer().print("successfully wrote directory {s}!\n", .{out_name}) catch {};
+
+            std.io.getStdOut().writer().print("successfully wrote directory {s}!\n", .{out_name.?}) catch {};
+
+            if (failed.items.len > 0) {
+                std.io.getStdOut().writer().print("failed demos:\n", .{}) catch {};
+                for (failed.items) |fail| {
+                    std.io.getStdOut().writer().print("    {s}\n", .{fail}) catch {};
+                }
+            }
         },
         else => |k| return err("bad file kind: {}", .{k}),
     }
@@ -67,8 +111,72 @@ pub fn main() u8 {
     return 0;
 }
 
-fn fixupDemo(in_file: *std.fs.File, out_file: *std.fs.File, ally: std.mem.Allocator) !void {
-    var buf_rd = std.io.bufferedReader(in_file.reader());
+fn fixupDir(
+    in_dir: std.fs.IterableDir,
+    out_dir: std.fs.Dir,
+    failed: *std.ArrayList([]const u8),
+    main_arena: std.mem.Allocator,
+) u8 {
+    const demo_count = countDirFiles(in_dir, main_arena) catch 0;
+
+    var progress: std.Progress = .{};
+
+    const root_prog_node = progress.start("run fixup", demo_count);
+    defer root_prog_node.end();
+
+    var dem_arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer dem_arena.deinit();
+
+    var walker = in_dir.walk(main_arena) catch |e| return err("failed to walk dir: {}", .{e});
+    defer walker.deinit();
+
+    while (walker.next() catch |e| return err("failed to iterate dir: {}", .{e})) |ent| {
+        if (ent.kind != .File) continue;
+
+        // create the output dir
+        if (std.fs.path.dirname(ent.path)) |dirname| {
+            out_dir.makePath(dirname) catch |e| return err("failed to make out dir {s}: {}", .{ dirname, e });
+        }
+
+        fixupFile(dem_arena.allocator(), root_prog_node, in_dir.dir, out_dir, ent.path, ent.path) catch |e| {
+            const str = std.fmt.allocPrint(main_arena, "{s}: {}", .{ ent.path, e }) catch return err("out of memory", .{});
+            failed.append(str) catch return err("out of memory", .{});
+        };
+
+        _ = dem_arena.reset(.retain_capacity);
+    }
+
+    return 0;
+}
+
+fn fixupFile(
+    dem_arena: std.mem.Allocator,
+    root_prog_node: *std.Progress.Node,
+    in_dir: std.fs.Dir,
+    out_dir: std.fs.Dir,
+    in_name: []const u8,
+    out_name: []const u8,
+) !void {
+    if (std.fs.path.dirname(out_name)) |dirname| {
+        try out_dir.makePath(dirname);
+    }
+
+    var in_data = try in_dir.readFileAlloc(dem_arena, in_name, 100_000_000);
+
+    var out_file = try out_dir.atomicFile(out_name, .{});
+    defer out_file.deinit();
+
+    {
+        var prog_node = root_prog_node.start("process demo", 1);
+        defer prog_node.end();
+        try fixupDemo(in_data, &out_file.file, dem_arena);
+    }
+
+    try out_file.finish();
+}
+
+fn fixupDemo(in_data: []const u8, out_file: *std.fs.File, ally: std.mem.Allocator) !void {
+    var buf_rd = std.io.fixedBufferStream(in_data);
     var buf_wr = std.io.bufferedWriter(out_file.writer());
 
     const in_rd = buf_rd.reader();
@@ -126,7 +234,7 @@ fn fixupDemo(in_file: *std.fs.File, out_file: *std.fs.File, ally: std.mem.Alloca
                 try out_wr.writeIntLittle(u32, size);
                 try clone(in_rd, out_wr, size);
             },
-            else => @panic("silly demo"),
+            else => return error.BadDemo,
         }
     }
 
